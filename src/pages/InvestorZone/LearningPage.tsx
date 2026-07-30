af1df6d546489289fa0acaf42ff4affbd69c3a9e
sonu7042 <sonusinghmourya100@gmail.com>
2026-07-30 18:09:46 +0530

    updated

--- src/pages/InvestorZone/LearningPage.tsx
+++ src/pages/InvestorZone/LearningPage.tsx
@@ -9,8 +9,6 @@ const videos = [
   "lAgaoNn0irQ",
 ];
 
-https://www.youtube.com/embed/wU7GLvvSDjI?si=lYWHwGTyO6bOhiNj'
-
 {
   /* <iframe width="560" height="315" src="https://www.youtube.com/embed/6dJmILRJU7E?si=-9h93VcRfxZ5e_yX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */
 }
@@ -36,7 +34,7 @@ const LearningPage: React.FC = () => {
       </div>
       {/* Heading */}
       <div className="mb-12">
-        <h2 className="text-3xl md:text-4xl font-bold mb-4">Learning</h2>
+        <h1 className="text-3xl md:text-4xl font-bold mb-4">Learning</h1>
         <p className="text-gray-600 max-w-3xl">
           Elaborate Capital Learning helps you understand investing and
           financial planning through simple video lessons, making finance easy
